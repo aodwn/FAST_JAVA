5cea3edefb0ca3fadaa2835e60cb16f515280a42
aodwn <74028304+aodwn@users.noreply.github.com>
2021-01-22 18:00:01 +0900

    Update StudentTest.java

--- Chapter5/src/reference/StudentTest.java
+++ Chapter5/src/reference/StudentTest.java
@@ -5,16 +5,16 @@ public class StudentTest {
 	public static void main(String[] args) {
 
 		Student studentLee = new Student(100, "Lee");
-		studentLee.setKoreaSubject("±¹¾î", 100);
-		studentLee.setMathSubject("¼öÇĞ", 95);
+		studentLee.setKoreaSubject("Â±Â¹Â¾Ã®", 100);
+		studentLee.setMathSubject("Â¼Ã¶Ã‡Ã", 95);
 		
 		
 		Student studentKim = new Student(101, "Kim");
-		studentKim.setKoreaSubject("±¹¾î", 80);
-		studentKim.setMathSubject("¼öÇĞ", 99);
+		studentKim.setKoreaSubject("Â±Â¹Â¾Ã®", 80);
+		studentKim.setMathSubject("Â¼Ã¶Ã‡Ã", 99);
 		
-		studentLee.showStudentSocre();
-		studentKim.showStudentSocre();
+		studentLee.showStudentScore();
+		studentKim.showStudentScore();
 		
 	}
 
